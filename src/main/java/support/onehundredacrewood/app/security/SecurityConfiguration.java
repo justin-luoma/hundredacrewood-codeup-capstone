a6f30cb6131bee1ec699d57808e596dd42e8c6ac
Justin Luoma <justin@justinluoma.com>
2018-11-09 16:18:14 -0600

    Require auth for posts/create

--- src/main/java/support/onehundredacrewood/app/security/SecurityConfiguration.java
+++ src/main/java/support/onehundredacrewood/app/security/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(
-                        "/ads/create", // only authenticated users can create ads
+                        "/posts/create", // only authenticated users can create ads
                         "/ads/{id}/edit" // only authenticated users can edit ads
                 )
                 .authenticated()
