83ac8b7cc6e6a4ec464cd17c5301f06c8ec51bd8
MichaelWaymire <mike.waymire2@gmail.com>
2018-11-09 10:18:43 -0600

    UserController started

---
+++ src/main/java/support/onehundredacrewood/app/controllers/UserController.java
@@ -0,0 +1,17 @@
+package support.onehundredacrewood.app.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class UserController {
+
+    @GetMapping("/profile")
+    public String showProfile() {
+        return "profile";
+    }
+
+
+}
