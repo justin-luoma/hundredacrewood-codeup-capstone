839518f7d49c2a6e9ba098846fab4ef436f74e68
Justin Luoma <justin@justinluoma.com>
2018-11-11 14:22:28 -0600

    Fixed resources page template link

--- src/main/java/support/onehundredacrewood/app/controllers/ResourcesController.java
+++ src/main/java/support/onehundredacrewood/app/controllers/ResourcesController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ResourcesController {
     @GetMapping("/resources")
     public String showResources() {
-        return "/resources";
+        return "resources";
     }
 }
