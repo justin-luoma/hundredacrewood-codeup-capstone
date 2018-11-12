ba74e5277b7d9b4b3c61d67329775bc9cd4204d0
MichaelWaymire <mike.waymire2@gmail.com>
2018-11-12 09:49:57 -0600

    created desc to postRepo

--- src/main/java/support/onehundredacrewood/app/dao/repositories/PostRepo.java
+++ src/main/java/support/onehundredacrewood/app/dao/repositories/PostRepo.java
@@ -3,6 +3,10 @@ package support.onehundredacrewood.app.dao.repositories;
 import org.springframework.data.repository.CrudRepository;
 import support.onehundredacrewood.app.dao.models.Post;
 
+import java.util.List;
+
 public interface PostRepo extends CrudRepository<Post, Long> {
     Post findById(long id);
+
+    List<Post> getAllByIdExistsOrderByCreatedDesc();
 }
