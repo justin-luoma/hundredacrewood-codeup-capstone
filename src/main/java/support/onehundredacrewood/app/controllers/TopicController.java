79d7a7cc2f10d1fd7b09de7fca848f1c6df2aaaa
MichaelWaymire <mike.waymire2@gmail.com>
2018-11-12 09:49:57 -0600

    created desc to postRepo

--- src/main/java/support/onehundredacrewood/app/controllers/TopicController.java
+++ src/main/java/support/onehundredacrewood/app/controllers/TopicController.java
@@ -5,15 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import support.onehundredacrewood.app.dao.repositories.PostRepo;
 import support.onehundredacrewood.app.dao.repositories.TopicRepo;
 
 @Controller
 public class TopicController {
 
     private final TopicRepo topicRepo;
+    private final PostRepo postRepo;
 
-    public TopicController(TopicRepo topicRepo) {
+    public TopicController(TopicRepo topicRepo,PostRepo postRepo) {
         this.topicRepo = topicRepo;
+        this.postRepo = postRepo;
     }
 
     @GetMapping("/topic")
@@ -24,8 +27,8 @@ public class TopicController {
 
     @GetMapping("/topic/{id}")
     public String individualTopic(@PathVariable long id, Model model) {
-//        model.addAttribute("topics", topicRepo.findById(id));
-        model.addAttribute("id", id);
+        model.addAttribute("topic", topicRepo.findById(id));
+        model.addAttribute("posts", postRepo.getAllByIdExistsOrderByCreatedDesc().subList(0,2));
 
         return "topics/topic";
     }
