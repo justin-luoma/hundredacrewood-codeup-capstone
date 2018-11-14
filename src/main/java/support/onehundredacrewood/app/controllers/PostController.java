e945bf7b351bd8da2293e34b10206bb82b00e7a8
MichaelWaymire <mike.waymire2@gmail.com>
2018-11-14 10:59:04 -0600

    added follow post feature

--- src/main/java/support/onehundredacrewood/app/controllers/PostController.java
+++ src/main/java/support/onehundredacrewood/app/controllers/PostController.java
@@ -1,5 +1,7 @@
 package support.onehundredacrewood.app.controllers;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,7 @@ import support.onehundredacrewood.app.dao.models.Topic;
 import support.onehundredacrewood.app.dao.models.User;
 import support.onehundredacrewood.app.dao.repositories.PostRepo;
 import support.onehundredacrewood.app.dao.repositories.TopicRepo;
+import support.onehundredacrewood.app.dao.repositories.UserRepo;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -19,10 +22,13 @@ import java.util.List;
 public class PostController {
     private final PostRepo postRepo;
     private final TopicRepo topicRepo;
+    private final UserRepo userRepo;
 
-    public PostController(PostRepo postRepo, TopicRepo topicRepo) {
+    public PostController(PostRepo postRepo, TopicRepo topicRepo,
+                          UserRepo userRepo) {
         this.postRepo = postRepo;
         this.topicRepo = topicRepo;
+        this.userRepo = userRepo;
     }
 
     @GetMapping("/posts")
@@ -33,8 +39,18 @@ public class PostController {
 
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable long id, Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        boolean isFollowing = false;
+        if (auth != null && !(auth instanceof AnonymousAuthenticationToken) && auth.isAuthenticated()) {
+            User principal = ( User ) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User user = userRepo.findById(principal.getId()).get();
+            isFollowing = Post.isFollowing(user.getFollowedPosts(), id);
+        }
         Post post = postRepo.findById(id);
+
+
         model.addAttribute("post", post);
+        model.addAttribute("isFollowing", isFollowing);
         return "post/one";
     }
 
@@ -88,5 +104,18 @@ public class PostController {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("posts", postRepo.findAllByUserOrderByCreatedDesc(user));
         return "post/index";
+    }
+
+    @PostMapping("posts/follow")
+    public String followPosts(@RequestParam(name = "id") long id, @RequestHeader(value = "referer") final String referer) {
+        System.out.println(referer);
+        User principal = ( User ) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Post post = postRepo.findById(id);
+        User user = userRepo.findById(principal.getId()).get();
+
+        user.followPost(post);
+        postRepo.save(post);
+
+        return "redirect:" + referer;
     }
 }
